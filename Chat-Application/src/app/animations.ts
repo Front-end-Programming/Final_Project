4536590a9eb2a329ec1f39d4b46e752063c315e1
Nguyen Hoang Duc Duy <18130059@st.hcmuaf.edu.vn>
2021-08-03 22:04:59 +0700

    Fix animation in sign-in, sign-up form

--- Chat-Application/src/app/animations.ts
+++ Chat-Application/src/app/animations.ts
@@ -15,13 +15,13 @@ export const fader = trigger('faderAnimation', [
         opacity: 0,
         transform: 'scale(0) translateY(100%)',
       }),
-    ]),
+    ], {optional: true}),
 
     query(':enter', [
       animate(
         '300ms ease',
-        style({ opacity: 1, transform: 'scale(1) translateY(0)' })
+        style({ opacity: 1, transform: 'scale(1) translateY(0)' }),
       ),
-    ]),
+    ], {optional: true}),
   ]),
 ]);
