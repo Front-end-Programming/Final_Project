f0d9d4128855b9e7948b05fa045a8efb0f0f2c3a
Nguyen Hoang Duc Duy <18130059@st.hcmuaf.edu.vn>
2021-08-30 22:49:30 +0700

    Delete unused components

--- Chat-Application/src/app/app.module.ts
+++ Chat-Application/src/app/app.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './Giang/header/header.component';
-import { ManHinhChinhComponent } from './Giang/man-hinh-chinh/man-hinh-chinh.component';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    ManHinhChinhComponent,
-    SignUpComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
