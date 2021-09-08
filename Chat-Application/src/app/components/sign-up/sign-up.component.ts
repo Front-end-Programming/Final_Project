450fec3190c252baf5422fc93f4bd758aba04ad1
NguyenThiGiang-18130061 <18130061@st.hcmuaf.edu.vn>
2021-09-08 21:25:58 +0700

    update responsive

--- Chat-Application/src/app/components/sign-up/sign-up.component.ts
+++ Chat-Application/src/app/components/sign-up/sign-up.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SignUpComponent implements OnInit {
   username: string = '';
   password: string = '';
+  confirmPass: string='';
   isShowPassword: boolean = false;
 
   // Kiem tra ten nguoi dung da ton tai hay chua
